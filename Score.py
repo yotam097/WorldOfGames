0e33aa57a4764dfbef355a91c85ac49411f54c09
yotam097 <68535738+yotam097@users.noreply.github.com>
2020-09-21 09:05:14 +0300

    Update Score.py

--- Score.py
+++ Score.py
@@ -5,20 +5,22 @@ global points, difficulty, last_score
 conn = pymysql.connect(host='192.168.99.100', port=3306, user='root', password='12345', db='Games')
 
 
-# This function writes and updates to the SQL database the current user & score 
 def add_score_db(name, points):
-    conn.autocommit(True)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Games.Users_Scores;")
-    # cursor.execute("DELETE FROM Games.Users_Scores WHERE User = 'John'")
-    for row in cursor:
-        if name in row:
-            new_score = points + row[1]
-            cursor.execute("UPDATE Games.Users_Scores SET Score = %s WHERE User = %s", (new_score, name))
-        else:
-            cursor.execute("INSERT into Games.Users_Scores (User, Score) VALUES (%s, %s)", (name, points))
-    cursor.close()
-    conn.close()
+    try:
+        conn.autocommit(True)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Games.Users_Scores;")
+        # cursor.execute("DELETE FROM Games.Users_Scores WHERE User = 'John'")
+        for row in cursor:
+            if name in row:
+                new_score = points + row[1]
+                cursor.execute("UPDATE Games.Users_Scores SET Score = %s WHERE User = %s", (new_score, name))
+            else:
+                cursor.execute("INSERT into Games.Users_Scores (User, Score) VALUES (%s, %s)", (name, points))
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print(e, '\n', "** Please start over with a different username.. **")
 
 
 # This function reads the previews score from the Scores file if exist, if not, it creates a new one and adding the
@@ -44,5 +46,5 @@ def add_score(points):
         file_overwrite.write(str(outp))
         print("Your score is: ", outp)
 
-        
+
 add_score_db("John", 0)
